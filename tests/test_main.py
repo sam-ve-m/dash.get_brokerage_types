cadb9eec2d9d69ad654781c122592f9da17a82c3
João Bonfim <joao.bonfim@lionx.com.br>
2022-12-13 09:28:11 -0300

    feat: endpoint to get brokerage types

---
+++ tests/test_main.py
@@ -0,0 +1,36 @@
+from unittest.mock import patch
+
+from main import get_enums
+from src.repository.brokerage_type_enum.repository import BrokerageTypeEnumRepository
+from src.service.brokerage_type_enum.service import BrokerageTypeEnumService
+from tests.test_doubles.doubles import (
+    main_service_response_dummy,
+    main_response_dummy,
+    enum_service_get_enums_response_none,
+    enum_service_response_none,
+    enum_service_response_invalid,
+)
+
+service_response_dummy = main_service_response_dummy
+
+
+@patch.object(BrokerageTypeEnumService, "get_response")
+def test_response_when_is_all_ok(get_response_mock):
+    get_response_mock.return_value = service_response_dummy
+    response = get_enums()
+    expected_response = main_response_dummy
+    assert response.data == expected_response
+
+
+@patch.object(BrokerageTypeEnumRepository, "get_brokerage_type_enum")
+def test_get_response_when_enums_are_none(get_enums_mock):
+    get_enums_mock.return_value = enum_service_get_enums_response_none
+    result = get_enums()
+    assert result.data == enum_service_response_none.encode()
+
+
+@patch.object(BrokerageTypeEnumRepository, "get_brokerage_type_enum")
+def test_get_response_when_enums_are_invalid(get_enums_mock):
+    get_enums_mock.side_effect = Exception("Erroooooou!")
+    result = get_enums()
+    assert result.data == enum_service_response_invalid.encode()
